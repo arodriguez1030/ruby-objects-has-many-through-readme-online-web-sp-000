5066035787a43d52282d59ef78e66c0418239d90
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-19 01:50:03 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,5 +20,10 @@ def new_meal(customer, total, tip=0)
     Meal.all.select do |meal|
       meal.waiter == self
     end
+    
+    def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
   end
 end
